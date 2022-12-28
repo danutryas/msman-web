07da142792937260e333ac1d36a5cf91b7d31a22
Danu Tryas <Danupristowo09@gmail.com>
2022-12-28 15:54:22 +0700

    vercel test

--- src/API/axios.tsx
+++ src/API/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
+    baseURL: "https://api.themoviedb.org/3",
     // withCredentials: true,
     headers : {
         'Content-Type': 'application/json'
